f8e8e8618576f5a4d3fd7e9a1326103397f022f2
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-09 12:05:10 -0700

    Add views for locations and events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,27 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to location_path
+    else
+      render :new
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def edit
   end
 
+  private
+    def event_params
+      params.require(:event).permit(:description, :date, :requires_id, :location_id)
+    end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,33 @@
 class LocationsController < ApplicationController
   def index
+    @locations = Location.all
+    location = Location.find(params[:id])
   end
 
   def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 
   def edit
+    @location = Location.find(params[:id])
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:name, :address, :zipcode)
   end
 end
